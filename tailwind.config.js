4e3ba8f8c64b8c393c44b3c80071858d571cb9e2
satya2215101040 <satya.sudharsan@student.undiksha.ac.id>
2023-10-01 03:19:41 +0800

    tugas web portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: { 
+      center: true,
+    padding: '16px',
+  },
+    extend: {
+      colors : {
+        primary: '#0ea5e9',
+        onemary: '#64748b',
+        dark: '#020617',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
+
